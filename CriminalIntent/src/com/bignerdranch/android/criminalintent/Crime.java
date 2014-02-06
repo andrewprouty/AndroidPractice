01e33ae231e38b6a1e4a1741439373204bd40839
andrewprouty <andyprouty@gmail.com>
2014-02-06 09:38:37 -0800

    chapter 8 end

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,15 +1,19 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+	private Boolean mSolved;
 
 	public Crime() {
 		// Generate unique identifier
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public UUID getId() {
@@ -22,6 +26,22 @@ public class Crime {
 
 	public void setTitle(String title) {
 		mTitle = title;
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public Boolean getSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(Boolean solved) {
+		mSolved = solved;
 	}
 
 }
