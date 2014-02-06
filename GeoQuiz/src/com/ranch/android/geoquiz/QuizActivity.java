eca572c4a53ceada5a4e8e8a7a111dfe3f60624c
andrewprouty <andyprouty@gmail.com>
2014-02-05 20:30:27 -0800

    starting chapter 7

--- GeoQuiz/src/com/ranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/ranch/android/geoquiz/QuizActivity.java
@@ -1,7 +1,10 @@
 package com.ranch.android.geoquiz;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -70,11 +73,19 @@ public class QuizActivity extends Activity {
 		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
 	}
 
+	@TargetApi(11)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			ActionBar actionBar = getActionBar();
+			actionBar.setSubtitle("Bodies of Water");
+		}
+		
+		mIsCheater = false;
 
 		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
